e5262942a5d51d39e6128189c1e5fed8de1faf30
Oscar Hoekman <oscar@Mac-Pro.local>
2015-02-16 10:59:36 -0800

    Solved Problem 6

---
+++ 6-Sum Square Difference.py
@@ -0,0 +1,15 @@
+#megatheriumNinja
+import Functions as functions
+
+num1 = 0
+num2 = 0
+
+for i in range(1, 101):
+	num1 += i
+
+num1 = num1 ** 2
+
+for i in range(1, 101):
+	num2 += i ** 2
+
+print(num1 - num2)
