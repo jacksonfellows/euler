6e19b2f0131d28df7ee5e12c5c305171c4406673
Jackson Fellows <jackson@fellowsfamily.com>
2019-03-17 11:29:36 -0700

    solved 35 with F#

---
+++ fsharp/35_Circular_primes.fs
@@ -0,0 +1,41 @@
+module CircularPrimes
+
+let erat n : bool array =
+    let (primes : bool array) = Array.create (n+1) true
+    primes.[0] <- false
+    primes.[1] <- false
+    let mutable p = 2
+    while p < int (sqrt (double n)) + 1 do
+        for i in p+p..p..n do
+            primes.[i] <- false
+        p <- p+1
+    primes
+
+// use this function to check our sieve against https://primes.utm.edu/howmany.html
+let primesUnder n = Array.filter id (erat n) |> Array.length
+
+let ps = erat 1000000
+let isPrime n = ps.[n]
+
+let rots n =
+    let len i = double i |> log10 |> floor |> int
+    let rec go (x,l) =
+        // TODO: combine into one expression (like divRem in Haskell)
+        let lsd = x % 10
+        let rest = x / 10
+        let next = lsd * (pown 10 l) + rest
+        if next <> n then
+            next :: go (next,l)
+        else []
+    n :: go (n,len n)
+
+let isCirc n = rots n |> List.forall isPrime
+let numCircs =
+    [1..1000000]
+    |> List.filter isCirc
+    |> List.length
+
+[<EntryPoint>]
+let main argv =
+    numCircs |> printf "there are %d circular primes below one million\n"
+    0
