9a54395c796e8e0382230dab409682a35c9b1292
Jackson Fellows <jackson@fellowsfamily.com>
2019-03-24 23:47:44 -0700

    solved 38

---
+++ haskell/38_Pandigital_multiples.hs
@@ -0,0 +1,14 @@
+import Data.List (inits, permutations)
+
+isConcatProd :: String -> Bool
+isConcatProd digits = any works (tail $ inits $ digits)
+  where works i = any (== digits) $ takeWhile
+          (\ds -> length ds <= length digits) $
+          concat <$> (flip take) prods <$> [2..]
+          where prods = (\n -> show (n * read i)) <$> [1..]
+
+pandigitals :: [String]
+pandigitals = permutations $ concatMap show [1..9]
+
+main :: IO ()
+main = print $ maximum $ filter isConcatProd pandigitals
