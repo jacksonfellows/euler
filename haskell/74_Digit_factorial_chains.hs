9a266432bdd4f71e95d82ebd81b38d0d0c9a79e6
Jackson Fellows <jtfellows@icloud.com>
2020-05-15 10:04:55 -0700

    added type annotations and replaced fact with unsafe array lookup
    still slower than C, not sure what else I should change

--- haskell/74_Digit_factorial_chains.hs
+++ haskell/74_Digit_factorial_chains.hs
@@ -1,21 +1,19 @@
-factorial 0 = 1
-factorial 1 = 1
-factorial 2 = 2
-factorial 3 = 6
-factorial 4 = 24
-factorial 5 = 120
-factorial 6 = 720
-factorial 7 = 5040
-factorial 8 = 40320
-factorial 9 = 362880
-factorial _ = error "factorial only defined for digits 0-9"
--- factorial = product . enumFromTo 1
+import Data.Array.Unboxed
+import Data.Array.Base (unsafeAt)
 
+facts :: UArray Int Int
+facts = array (0,9) [(0,1), (1,1), (2,2), (3,6), (4,24), (5,120), (6,720), (7,5040), (8,40320), (9,362880)]
+
+factorial :: Int -> Int
+factorial = unsafeAt facts
+
+digitFactorialSum :: Int -> Int
 digitFactorialSum = go 0
   where go acc n = case n `divMod` 10 of
                      (0,r) -> acc + factorial r
                      (q,r) -> go (acc + factorial r) q
 
+chainLength :: Int -> Int
 chainLength = go 0
   where go acc n = case n of
                      1 -> acc + 1
@@ -31,6 +29,8 @@ chainLength = go 0
                      40585 -> acc + 1
                      otherwise -> go (acc + 1) $ digitFactorialSum n
 
+chains :: [Int]
 chains = [ n | n <- [1..1000000], chainLength n == 60 ]
 
+main :: IO ()
 main = print $ length chains
