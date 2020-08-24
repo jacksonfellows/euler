a82d9e8228665e2cd7e8248ad57b1e173f018b8c
Jackson Fellows <jtfellows@icloud.com>
2020-08-24 08:23:35 -0400

    use Binet's formula to approximate first 9 digits
    for some reason, must use log10 instead of log for enough precision

--- python/104_Pandigital_Fibonacci_ends.py
+++ python/104_Pandigital_Fibonacci_ends.py
@@ -1,17 +1,18 @@
-import gmpy2
+from math import *
 
 def pandigital(n):
     return sorted(str(n)) == ['1','2','3','4','5','6','7','8','9']
 
 def p104():
-    a = b = gmpy2.mpz(1)
+    a = b = 1
     n = 1
+    logPhi = log10(1 + sqrt(5)) - log10(2)
+    logF = logPhi - log10(sqrt(5))
     while 1:
-        if n % 10000 == 0:
-            print(n)
-        if pandigital(a // 10**(a.num_digits() - 9)) and pandigital(a % 1000000000):
+        if pandigital(a) and pandigital(int(exp(log(10)*logF)*1e8)):
             return n
-        a, b = b, a + b
+        a, b = b, (a + b)%1000000000
+        logF = (logF + logPhi)%1
         n += 1
 
 print(p104())
