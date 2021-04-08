a8a3cac8e39b842266e0877b4fd465d2a81da9c5
Jackson Fellows <jtfellows@icloud.com>
2021-04-08 12:52:08 -0400

    a bit simpler, but no faster

--- python/122_Efficient_exponentiation.py
+++ python/122_Efficient_exponentiation.py
@@ -1,24 +1,27 @@
-from functools import lru_cache
+ms = {1:[set()]}
 
-@lru_cache(None)
-def m(k):
-    if k == 1:
-        return (frozenset(),)
+def fill_row(k):
     mins = []
     min_len = float('inf')
     for i in range(1,k//2+1):
-        for a in m(i):
-            for b in m(k-i):
+        for a in ms[i]:
+            for b in ms[k-i]:
                 x = a | b
                 len_x = len(x)
                 if len_x < min_len:
                     min_len = len_x
+                    x.add(k)
                     mins = [x]
                 elif len_x == min_len:
+                    x.add(k)
                     mins.append(x)
-    return tuple(frozenset((k,)) | min_ for min_ in mins)
+    ms[k] = mins
+    return 1 + min_len
 
 def p122():
-    return sum(len(m(k)[0]) for k in range(1,201))
+    s = 0
+    for k in range(2,201):
+        s += fill_row(k)
+    return s
 
 print(p122())
